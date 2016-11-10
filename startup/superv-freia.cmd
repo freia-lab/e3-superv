02d00c1c6bdd38d5efb0d28f123d4bf5b40b82b0
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2016-11-10 17:04:33 +0100

    The first test version.

--- startup/superv-freia.cmd
+++ startup/superv-freia.cmd
@@ -5,12 +5,15 @@ epicsEnvSet("D", "UCD-SNMP-MIB::")
 epicsEnvSet("W", "SNMPv2-MIB::")
 epicsEnvSet("N", "NOTIFICATION-LOG-MIB::")
 
-epicsEnvSet("IPADDR1", $(HOST_1))
-devSnmpSetSnmpVersion($(HOST_1),SNMP_VERSION_1)
+epicsEnvSet("HOST_1" "$(SUPERV_FREIA_1=192.168.10.99)")
+
+devSnmpSetSnmpVersion($(HOST_1),SNMP_VERSION_2c)
 
 #devSnmpSetParam("DebugLevel",10)
 
-dbLoadRecords("superv-freia.db","H=$(HOST_1)"
+# HOST_1 
+dbLoadRecords("diskTable.template","P=Ctrl-freiavm2,PART=disk-root,PASSWD=freia_secret,H=$(HOST_1),CHAN=1")
+dbLoadRecords("hostSuperv.template","P=Ctrl-freiavm2,PASSWD=freia_secret,H=$(HOST_1)")
 
 #requireSnippet(dbToLoad-superv-freia.cmd, "H_IPADDR=$(HOST_1)")
 
