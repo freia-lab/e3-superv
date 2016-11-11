c8261293669080e326288d8257b8d95542246a7f
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2016-11-11 16:54:38 +0100

    Added more computers to supervise.

--- startup/superv-freia.cmd
+++ startup/superv-freia.cmd
@@ -5,17 +5,53 @@ epicsEnvSet("D", "UCD-SNMP-MIB::")
 epicsEnvSet("W", "SNMPv2-MIB::")
 epicsEnvSet("N", "NOTIFICATION-LOG-MIB::")
 
-epicsEnvSet("HOST_1" "$(SUPERV_FREIA_1=192.168.10.99)")
+epicsEnvSet("HOST_1" "$(SUPERV_FREIA_1=192.168.10.60)")
+epicsEnvSet("HOST_2" "$(SUPERV_FREIA_2=192.168.10.62)")
+epicsEnvSet("HOST_3" "$(SUPERV_FREIA_3=192.168.10.64)")
+epicsEnvSet("HOST_4" "$(SUPERV_FREIA_4=192.168.10.107)")
+epicsEnvSet("HOST_5" "$(SUPERV_FREIA_5=192.168.10.108)")
+epicsEnvSet("HOST_6" "$(SUPERV_FREIA_6=192.168.10.120)")
 
 devSnmpSetSnmpVersion($(HOST_1),SNMP_VERSION_2c)
+devSnmpSetSnmpVersion($(HOST_2),SNMP_VERSION_2c)
+devSnmpSetSnmpVersion($(HOST_3),SNMP_VERSION_2c)
+devSnmpSetSnmpVersion($(HOST_4),SNMP_VERSION_2c)
+devSnmpSetSnmpVersion($(HOST_5),SNMP_VERSION_2c)
+devSnmpSetSnmpVersion($(HOST_6),SNMP_VERSION_2c)
 
 #devSnmpSetParam("DebugLevel",10)
 
-# HOST_1 
-dbLoadRecords("diskTable.template","P=Ctrl-freiavm2,PART=disk-root,PASSWD=freia_secret,H=$(HOST_1),CHAN=1")
-dbLoadRecords("hostSuperv.template","P=Ctrl-freiavm2,PASSWD=freia_secret,H=$(HOST_1)")
+# HOST_1 (freia-srv1)
+dbLoadRecords("diskTable.template","P=Ctrl-freiaSrv1,PART=disk-root,PASSWD=freia_secret,H=$(HOST_1),CHAN=1")
+dbLoadRecords("diskTable.template","P=Ctrl-freiaSrv1,PART=disk-data1,PASSWD=freia_secret,H=$(HOST_1),CHAN=7")
+dbLoadRecords("hostSuperv.template","P=Ctrl-freiaSrv1,PASSWD=freia_secret,H=$(HOST_1)")
 
-#requireSnippet(dbToLoad-superv-freia.cmd, "H_IPADDR=$(HOST_1)")
+# HOST_2 (centos-srv1)
+dbLoadRecords("diskTable.template","P=Ctrl-centosSrv1,PART=disk-root,PASSWD=freia_secret,H=$(HOST_2),CHAN=1")
+dbLoadRecords("diskTable.template","P=Ctrl-centosSrv1,PART=disk-boot,PASSWD=freia_secret,H=$(HOST_2),CHAN=2")
+dbLoadRecords("diskTable.template","P=Ctrl-centosSrv1,PART=disk-data1,PASSWD=freia_secret,H=$(HOST_2),CHAN=3")
+dbLoadRecords("hostSuperv.template","P=Ctrl-centosSrv1,PASSWD=freia_secret,H=$(HOST_2)")
+
+# HOST_3 (freia-arcapl1)
+dbLoadRecords("diskTable.template","P=Ctrl-arcApl1,PART=disk-root,PASSWD=freia_secret,H=$(HOST_3),CHAN=1")
+dbLoadRecords("diskTable.template","P=Ctrl-arcApl1,PART=disk-boot,PASSWD=freia_secret,H=$(HOST_3),CHAN=2")
+dbLoadRecords("diskTable.template","P=Ctrl-arcApl1,PART=disk-data1,PASSWD=freia_secret,H=$(HOST_3),CHAN=3")
+dbLoadRecords("hostSuperv.template","P=Ctrl-arcApl1,PASSWD=freia_secret,H=$(HOST_3)")
+
+# HOST_4 (vm-codac-1)
+dbLoadRecords("diskTable.template","P=Ctrl-vmCodac1,PART=disk-root,PASSWD=freia_secret,H=$(HOST_4),CHAN=1")
+dbLoadRecords("diskTable.template","P=Ctrl-vmCodac1,PART=disk-boot,PASSWD=freia_secret,H=$(HOST_4),CHAN=2")
+dbLoadRecords("hostSuperv.template","P=Ctrl-vmCodac1,PASSWD=freia_secret,H=$(HOST_4)")
+
+# HOST_5 (freia-arch1)
+dbLoadRecords("diskTable.template","P=Ctrl-freiaArch1,PART=disk-root,PASSWD=freia_secret,H=$(HOST_5),CHAN=1")
+dbLoadRecords("diskTable.template","P=Ctrl-freiaArch1,PART=disk-boot,PASSWD=freia_secret,H=$(HOST_5),CHAN=2")
+dbLoadRecords("diskTable.template","P=Ctrl-freiaArch1,PART=disk-data1,PASSWD=freia_secret,H=$(HOST_5),CHAN=3")
+dbLoadRecords("hostSuperv.template","P=Ctrl-freiaArch1,PASSWD=freia_secret,H=$(HOST_5)")
+
+# HOST_6 ()
+#dbLoadRecords("diskTable.template","P=Ctrl-freiavm2,PART=disk-root,PASSWD=freia_secret,H=$(HOST_6),CHAN=1")
+#dbLoadRecords("hostSuperv.template","P=Ctrl-freiavm2,PASSWD=freia_secret,H=$(HOST_6)")
 
 #############################################
 ## IOC initialization                      ##
